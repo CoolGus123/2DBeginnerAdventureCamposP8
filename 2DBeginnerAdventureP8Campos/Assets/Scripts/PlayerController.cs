4efa2a8c9dae92c8db61328f90a10620bf4e0260
CoolGus123 <gustavo4022010@gmail.com>
2024-11-01 15:24:59 -0700

    i moved on decoration
    i moved to decoration so i dont get behind but i maked the tile allittle bit easier so i wont stress about that map i havent finished a long time ago i will try to move up and catch up so i can bring my grades up

--- 2DBeginnerAdventureP8Campos/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Campos/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+    
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-       
+       rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -15,11 +23,27 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
         Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
         
+        transform.position = position;
     }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+
+
+    
 }
