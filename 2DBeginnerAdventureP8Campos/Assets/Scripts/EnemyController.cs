cfaee0258ab5bac413246c6f45d1df2d5760d727
CoolGus123 <gustavo4022010@gmail.com>
2024-11-18 15:19:40 -0800

    aniamation
    i animated thew character  and robot ofd 2d begiinner ADVENTUERE

--- 2DBeginnerAdventureP8Campos/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Campos/Assets/Scripts/EnemyController.cs
@@ -4,25 +4,60 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;  
 
     Rigidbody2D rigidbody2;
+
+    float timer;
+    int direction = 1;
+
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2 = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
     }
     void FixedUpdate()
     {
         Vector2 position = rigidbody2.position;
-        position.x = position.x + Time.deltaTime * speed;
+        if (vertical)
+        {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+
 
         rigidbody2.MovePosition(position);
     }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
 }
--- 2DBeginnerAdventureP8Campos/Assets/Scripts/RoxyController.cs
+++ 2DBeginnerAdventureP8Campos/Assets/Scripts/RoxyController.cs
@@ -17,12 +17,14 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-    // Start is called before the first frame update
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     void Start()
     {
-       rigidbody2d = GetComponent<Rigidbody2D>();
-       currentHealth = maxHealth;
-       
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,11 +32,24 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = transform.position;
         position.x = position.x + 4.0f * horizontal * Time.deltaTime;
         position.y = position.y + 4.0f * vertical * Time.deltaTime;
-        
+
         transform.position = position;
+
 
         if (isInvincible)
         {
@@ -57,6 +72,8 @@ public class PlayerController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if (isInvincible)
             {
                 return;
